bc7a5c4acd67c03e3079153fc10c7f7855e6bf17
Jessie Roeder <36315040+JessieRoeder@users.noreply.github.com>
2025-04-03 02:22:42 -0400

    making progress, switching away from separate subsystem for LL

--- src/main/java/frc/robot/autos/LimelightCoralAuto.java
+++ src/main/java/frc/robot/autos/LimelightCoralAuto.java
@@ -4,19 +4,24 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.RollerConstants;
+import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.RollerSubsystem;
-import frc.robot.subsystems.LimelightSubsystem;
 
 public class LimelightCoralAuto extends Command {
     private DriveSubsystem m_drive;
     private RollerSubsystem m_roller;
     private ArmSubsystem m_arm;
-    private LimelightSubsystem m_vision;
     private Timer timer;
 
     private double auto_seconds = 15;
+
+    // These numbers must be tuned for your Robot!  Be careful!
+    final double STEER_K = 0.03;                    // how hard to turn toward the target
+    final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+    final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
+    final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
 
     /**
      * This auto will have the robot drive forwards, stop, then drop the coral into L1
@@ -24,20 +29,20 @@ public class LimelightCoralAuto extends Command {
      * @param drive
      * @param roller
      * @param arm
-     * @param vision
      */
-    public LimelightCoralAuto(DriveSubsystem drive, RollerSubsystem roller, ArmSubsystem arm, LimelightSubsystem vision)
+    public LimelightCoralAuto(DriveSubsystem drive, RollerSubsystem roller, ArmSubsystem arm)
     {
         m_drive = drive;
         m_roller = roller;
         m_arm = arm;
-        m_vision = vision;
         
         timer = new Timer();
 
         addRequirements(m_drive);
         addRequirements(m_roller);
         addRequirements(m_arm);
+
+        
     }
 
     @Override
@@ -55,22 +60,15 @@ public class LimelightCoralAuto extends Command {
      */ 
     m_arm.runArm(ArmConstants.ARM_HOLD_UP);
 
-    /**
-     * While this timer is less than drive_seconds, the robot will obey the command inside
-     */
-    if(timer.get() < drive_seconds)
-    {
-        m_drive.driveArcade(-0.3, 0.0,false);
-    }
-    /**
-     * Once the timer is greater than drive_seconds but less than exjest seconds,
-     * the code inside will run, here we stop the drivetrain and exjest the coral.
-     */
-    else if(timer.get() > drive_seconds && timer.get() < exjest_seconds)
-    {
-        m_drive.driveArcade(0.0, 0.0,false);
-        m_roller.runRoller(RollerConstants.ROLLER_CORAL_OUT);
-    }
+    //get the LimeLight data
+    double tx = LimelightHelpers.getTX("");  // Horizontal offset from crosshair to target in degrees
+    double ty = LimelightHelpers.getTY("");  // Vertical offset from crosshair to target in degrees
+    double ta = LimelightHelpers.getTA("");  // Target area (0% to 100% of image)
+    boolean hasTarget = LimelightHelpers.getTV(""); // Do you have a valid target?
+
+    double txnc = LimelightHelpers.getTXNC("");  // Horizontal offset from principal pixel/point to target in degrees
+    double tync = LimelightHelpers.getTYNC("");  // Vertical  offset from principal pixel/point to target in degrees
+
   }
 
   // Runs each time the command ends via isFinished or being interrupted.
